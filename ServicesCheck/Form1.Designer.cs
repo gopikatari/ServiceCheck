3792d753b4b23243173459f8a919122b6b5dd3ab
Gopi Katari <katarigopi@gmail.com>
2019-07-05 09:41:32 +0530

    first commit

---
+++ ServicesCheck/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ServicesCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnStop.Location = new System.Drawing.Point(711, 105);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(145, 45);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.Green;
+            this.btnStart.Location = new System.Drawing.Point(586, 105);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(119, 45);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ServiceCheck";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnRestart.Location = new System.Drawing.Point(862, 105);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(119, 45);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "Re-Start";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            // 
+            // lstResult
+            // 
+            this.lstResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.ItemHeight = 25;
+            this.lstResult.Location = new System.Drawing.Point(180, 168);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(844, 125);
+            this.lstResult.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(38, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Monitoring :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(249, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 44);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 310);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Name = "Form1";
+            this.Text = "Services";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ServicesCheck/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+
+namespace ServicesCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string servicename;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            servicename = "PhoneSvc";
+            CheckServices();
+            checkServiceStatus(servicename);
+            label3.Text = servicename;
+        }
+
+        private void CheckServices()
+        {
+            if (serviceExists(servicename))
+            {
+                lstResult.Items.Add("service::" + servicename + " exists");
+            }
+            else
+            {
+                lstResult.Items.Add("Service doesn't exists");
+            }
+        }
+        //check Service exists
+        public bool serviceExists(string ServiceName)
+        {
+            return ServiceController.GetServices().Any(serviceController => serviceController.ServiceName.Equals(ServiceName));
+        }
+
+        public void checkServiceStatus(string service)
+        {
+            ServiceController sc = new ServiceController();
+            sc.ServiceName = service;
+
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                lstResult.Items.Add(string.Format("The {0} service status is currently set to {1}",
+                              service, sc.Status.ToString()));
+            }else if
+             (sc.Status == ServiceControllerStatus.Running)
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                lstResult.Items.Add(string.Format("The {0} service status is currently set to {1}",
+                              service, sc.Status.ToString()));
+            }
+            else if (sc.Status == ServiceControllerStatus.Paused)
+            {
+                lstResult.Items.Add(string.Format("The {0} service status is currently set to {1}",
+                              service, sc.Status.ToString()));
+            }
+        }
+
+        #region startservice
+        public void startService(string ServiceName)
+        {
+            ServiceController sc = new ServiceController();
+            sc.ServiceName = ServiceName;
+            //lstResult.Items.Add(string.Format("The {0} service status is currently set to {1}",                ServiceName, sc.Status.ToString()));
+            
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                // Start the service if the current status is stopped.
+                lstResult.Items.Add(string.Format("Starting the {0} service ...", ServiceName));
+
+                try
+                {
+                    // Start the service, and wait until its status is "Running".
+                    sc.Start();
+                    sc.WaitForStatus(ServiceControllerStatus.Running);
+
+                    // Display the current service status.
+                    lstResult.Items.Add(string.Format("The {0} service status is now set to {1}.", ServiceName, sc.Status.ToString()));
+                    MessageBox.Show("Service :"+ ServiceName + " started.");
+                }
+                catch (InvalidOperationException e)
+                {
+                    lstResult.Items.Add(string.Format("Could not start the {0} service.", ServiceName));
+                    lstResult.Items.Add(e.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Service:: {0} already running.", ServiceName));
+            }
+            checkServiceStatus(ServiceName);
+        }
+        #endregion
+
+
+        #region StopService
+        public void stopService(string ServiceName)
+        {
+            ServiceController sc = new ServiceController();
+            sc.ServiceName = ServiceName;
+
+            //Console.WriteLine("The {0} service status is currently set to {1}", ServiceName, sc.Status.ToString());
+
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                // Start the service if the current status is stopped.
+                lstResult.Items.Add(string.Format("Stopping the {0} service ...", ServiceName));
+               
+                try
+                {
+                    // Start the service, and wait until its status is "Running".
+                    sc.Stop();
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped);
+
+                    // Display the current service status.
+                    MessageBox.Show(string.Format("The {0} service status is now set to {1}.", ServiceName, sc.Status.ToString()));
+                   // Console.WriteLine(string.Format("The {0} service status is now set to {1}.", ServiceName, sc.Status.ToString()));
+                }
+                catch (InvalidOperationException e)
+                {
+                    lstResult.Items.Add(string.Format("Could not stop the {0} service.", ServiceName));
+                    MessageBox.Show(e.Message);
+                    //Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Cannot stop service {0} because it's already inactive.", ServiceName);
+            }
+            checkServiceStatus(ServiceName);
+        }
+        #endregion
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startService(servicename);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stopService(servicename);
+        }
+    }
+}
